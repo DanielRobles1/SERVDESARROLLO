da5df11ca8e72282071093aed7c152e1dd91f412
DanielRobles1 <162934388+DanielRobles1@users.noreply.github.com>
2024-11-16 16:38:23 -0600

    venta1

--- src/models/vent.js
+++ src/models/vent.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const ventSchema = new mongoose.Schema({
+
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    idVenta: { type: String, required: true, unique: true },
     producto: { type: String, required: true },
     cantidad: { type: Number, required: true },
     precioUnitario: { type: Number, required: true },
@@ -9,4 +11,10 @@ const ventSchema = new mongoose.Schema({
     fecha: { type: Date, default: Date.now }
 });
 
+ventSchema.pre('save', function (next) {
+    if (!this.unidVenta) {
+        this.unidVenta = `VENTA-${Date.now()}`;
+    }
+    next();
+});
 module.exports = mongoose.model('vent', ventSchema);
