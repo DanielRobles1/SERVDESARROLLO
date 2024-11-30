1bdf0f0513706c2b8e1b31b2b7fbe4ff2f172636
DanielRobles1 <162934388+DanielRobles1@users.noreply.github.com>
2024-11-30 14:31:57 -0600

    h

--- src/middleware/ipMiddleware.js
+++ src/middleware/ipMiddleware.js
@@ -6,11 +6,10 @@ const verifyGeolocation = async (req, res, next) => {
         console.log('Middleware verifyGeolocation ejecutándose...');
 
         const userIp = req.headers['x-forwarded-for']
-    ? req.headers['x-forwarded-for'].split(',')[0].trim() // Primera IP del encabezado
-    : req.connection.remoteAddress;
+            ? req.headers['x-forwarded-for'].split(',')[0].trim() // Primera IP del encabezado
+            : req.connection.remoteAddress;
 
-console.log('IP enviada a ipstack:', userIp);
-
+        console.log('IP enviada a IPinfo.io:', userIp);
 
         // Si estamos en local, saltar validación de geolocalización
         if (userIp === '::1' || userIp === '127.0.0.1') {
@@ -18,17 +17,18 @@ console.log('IP enviada a ipstack:', userIp);
             return next();
         }
 
-        // Hacer una solicitud a ipstack para verificar la geolocalización de la IP
+        // Hacer una solicitud a IPinfo.io para verificar la geolocalización de la IP
         const response = await axios.get(
-            `http://api.ipstack.com/${userIp}?access_key=${process.env.IPSTACK_API_KEY}`
+            `https://ipinfo.io/${userIp}?token=${process.env.IPINFO_API_KEY}`
         );
-        console.log('Respuesta completa de ipstack:', response.data);
-        
+
+        console.log('Respuesta completa de IPinfo.io:', response.data);
+
         // Extraer el código de país
-        const { country_code } = response.data;
+        const { country } = response.data;
 
         // Verificar si el país es México
-        if (country_code !== 'MX') {
+        if (country !== 'MX') {
             console.log('Acceso denegado: La IP no pertenece a México.');
             return res.status(403).json({ message: 'Acceso denegado, solo se permite el acceso desde México' });
         }
@@ -39,7 +39,8 @@ console.log('IP enviada a ipstack:', userIp);
         next(); // Continuar al siguiente middleware o ruta
     } catch (error) {
         console.error('Error al verificar la ubicación:', error.message);
-        res.status(500).json({ message: 'Error al verificar la ubicación' }); }
+        res.status(500).json({ message: 'Error al verificar la ubicación' });
+    }
 };
 
 module.exports = verifyGeolocation;
